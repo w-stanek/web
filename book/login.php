bcfda5cab0226e45db6f3aba7c4f28424199d6b9
w-stanek <93320171+w-stanek@users.noreply.github.com>
2021-12-10 18:26:13 +0100

    Update login.php

--- book/login.php
+++ book/login.php
@@ -1,6 +1,11 @@
-<?php 
-$servername = "md328.wedos.net";
-$username = "w287778_books";
-$password = "u5EJ58GJ";
-$dbname = "d287778_books";
+<?php 
+
+$servername = "";
+
+$username = "";
+
+$password = "";
+
+$dbname = "";
+
 ?>
